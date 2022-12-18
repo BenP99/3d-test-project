3c7101ce8fb3fd7d434508aa8c0ee21d5a90215e
Benjamin Pearce <ben.pearce25@gmail.com>
2022-12-18 19:35:24 +0000

    Game Build and Final Cube Spawn times

--- 3DTestProject/Assets/Scripts/SpawnerController.cs
+++ 3DTestProject/Assets/Scripts/SpawnerController.cs
@@ -45,11 +45,11 @@ public class SpawnerController : MonoBehaviour
         if(sc.lvl == 1) {
             time = 20;
         } else if(sc.lvl == 2) {
-            time = 18;
-        } else if(sc.lvl == 3) {
             time = 16;
-        } else if(sc.lvl == 4) {
+        } else if(sc.lvl == 3) {
             time = 14;
+        } else if(sc.lvl == 4) {
+            time = 10;
         }
     }
 
